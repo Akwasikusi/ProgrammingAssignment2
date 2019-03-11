24c3b012608f23e0933151bd79b86fc9d7be19aa
Akwasikusi <akwasikusi@gmail.com>
2019-03-10 22:59:08 -0500

    Read to push assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+rm(list=ls())
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
@@ -5,6 +7,23 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  
+    m <- NULL ## Create a null assignment 
+    #set the value of the matrix 
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  #  get the value of the matrix
+  get <- function() x
+  # set the value of the inverse
+  setsolve <- function(solve) m <<- solve
+  # get the value of the inverse
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 }
 
 
@@ -12,4 +31,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
